e281291ab57159aa08350923411809571f286628
waltertw <walterolivero@gmail.com>
2015-06-25 15:45:15 -0300

    add unbalance customers view

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -110,7 +110,7 @@ environments {
             host = "smtp.gmail.com"
             port = 465
             username = "walter.olivero@mercadolibre.com"
-            password = ""
+            password = " "
             props = ["mail.smtp.auth":"true",                     
                     "mail.smtp.socketFactory.port":"465",
                     "mail.smtp.socketFactory.class":"javax.net.ssl.SSLSocketFactory",
